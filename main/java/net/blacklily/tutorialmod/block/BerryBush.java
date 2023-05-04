4b69e028ba05bc1f90787271ab1cf7b9fbc1230a
BlackLilly96 <132537121+BlackLilly96@users.noreply.github.com>
2023-05-04 00:43:29 -0400

    Delete main directory

--- main/java/net/blacklily/tutorialmod/block/BerryBush.java
+++
@@ -1,9 +0,0 @@
-package net.blacklily.tutorialmod.block;
-
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-
-public class BerryBush extends HorizontalDirectionalBlock {
-    protected BerryBush(Properties properties) {
-        super(properties);
-    }
-}
