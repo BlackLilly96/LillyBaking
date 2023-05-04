e9338ed5892f129b55fe8c14168f1bd3f64dc855
BlackLilly96 <132537121+BlackLilly96@users.noreply.github.com>
2023-05-04 00:44:35 -0400

    Add files via upload

---
+++ main/java/net/blacklily/tutorialmod/block/BerryBush.java
@@ -0,0 +1,9 @@
+package net.blacklily.tutorialmod.block;
+
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+
+public class BerryBush extends HorizontalDirectionalBlock {
+    protected BerryBush(Properties properties) {
+        super(properties);
+    }
+}
